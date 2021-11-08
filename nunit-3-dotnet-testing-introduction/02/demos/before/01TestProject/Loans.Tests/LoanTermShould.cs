06cbc470161664ce85e59661f134ff493e6ff822
Agith-Ravn <76996657+Agith-Ravn@users.noreply.github.com>
2021-11-08 15:55:25 +0100

    .

---
+++ nunit-3-dotnet-testing-introduction/02/demos/before/01TestProject/Loans.Tests/LoanTermShould.cs
@@ -0,0 +1,37 @@
+﻿using Loans.Domain.Applications;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loans.Tests
+{
+    [TestFixture]
+    class LoanTermShould
+    {
+        [Test]
+        public void ReturnTermInMonths()
+        {
+            var sut = new LoanTerm(1);
+            Assert.That(sut.ToMonths(), Is.EqualTo(12));
+        }
+
+        [Test]
+        public void StoreYears()
+        {
+            var sut = new LoanTerm(1);
+            Assert.That(sut.Years, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void RespectValueEquality()
+        {
+            var a = 1;
+            var b = 1;
+
+            Assert.That(a, Is.EqualTo(b));
+        }
+    }
+}
